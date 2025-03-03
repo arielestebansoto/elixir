14e96ef0ac4776218f41c6275d3849c70534d5cd
Ariel Soto <arielestebansoto@gmail.com>
2025-03-03 14:26:00 -0300

    feat(app): Basic security setup

---
+++ elixir/src/main/java/com/arielsoto/elixir/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.arielsoto.elixir.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll();
+                    auth.anyRequest().denyAll();
+                })
+                .build();
+    }
+}
