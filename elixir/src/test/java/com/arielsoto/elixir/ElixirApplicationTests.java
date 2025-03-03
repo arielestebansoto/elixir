dcde29b5f3f7018eb2bb31568f50b692e4494db4
Ariel Soto <arielestebansoto@gmail.com>
2025-03-03 11:53:11 -0300

    feat(app): Project setup

---
+++ elixir/src/test/java/com/arielsoto/elixir/ElixirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arielsoto.elixir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElixirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
