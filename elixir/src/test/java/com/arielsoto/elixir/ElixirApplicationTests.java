2270fd0e9fed327dec4d868eb6d96db1a3a83f40
Ariel Soto <arielestebansoto@gmail.com>
2025-03-24 11:17:15 -0300

    feat(app): Implement a red test

--- elixir/src/test/java/com/arielsoto/elixir/ElixirApplicationTests.java
+++ elixir/src/test/java/com/arielsoto/elixir/ElixirApplicationTests.java
@@ -3,6 +3,7 @@ package com.arielsoto.elixir;
 import com.arielsoto.elixir.cocktail.common.domain.Cocktail;
 import com.arielsoto.elixir.cocktail.common.repository.ICocktailRepository;
 import com.arielsoto.elixir.config.EmbeddedMongoConfiguration;
+import com.arielsoto.elixir.config.TestSecurityConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,16 +14,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.BasicJsonTester;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-@Import(EmbeddedMongoConfiguration.class)
+@Import({EmbeddedMongoConfiguration.class, TestSecurityConfig.class})
 @AutoConfigureMockMvc
 class ElixirApplicationTests {
 
@@ -70,5 +73,17 @@ class ElixirApplicationTests {
 							"normalizedName": "mojito"
 						}
 				""");
+	}
+
+	@Test
+	void shoudlReturnAnauthorizedWhenCreatingCocktailWithoutAuth() throws Exception {
+		mockMvc.perform(post("/api/v1/cocktails/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("""
+					{
+						"name": "Mojito"
+					}
+				"""))
+				.andExpect(status().isForbidden());
 	}
 }
