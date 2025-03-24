d1f4b442625d84aa150485f1138c694614fb3a82
Ariel Soto <arielestebansoto@gmail.com>
2025-03-24 11:16:59 -0300

    feat(app): Configure OAuth2 for testing

--- elixir/src/main/java/com/arielsoto/elixir/config/SecurityConfig.java
+++ elixir/src/main/java/com/arielsoto/elixir/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.arielsoto.elixir.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,8 +16,10 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll();
+                    auth.requestMatchers(HttpMethod.POST, "api/v1/**").authenticated();
                     auth.anyRequest().denyAll();
                 })
+                .oauth2Login(Customizer.withDefaults())
                 .build();
     }
 }
